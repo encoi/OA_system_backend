69d566b4f4c303d9315d7c9bceed9388f834e1ff
ZhangXu <zxkl1314@gmail.com>
2024-05-20 11:11:39 +0800

    Initial commit

---
+++ src/main/java/com/example/oa_system_backend/OaSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.oa_system_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OaSystemBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OaSystemBackendApplication.class, args);
+    }
+
+}
